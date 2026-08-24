812b3d09970dc6121fda5b0e057c23e44e286f1f
ahmadpourostami <103726857+ahmadpourostami@users.noreply.github.com>
2026-08-24 07:27:28 +0330

    chore: add safe uninstall handler

---
+++ uninstall.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Irani LMS uninstall handler.
+ *
+ * Destructive data removal is intentionally disabled in the first release.
+ * A future setting will allow administrators to explicitly remove LMS data.
+ */
+
+defined( 'WP_UNINSTALL_PLUGIN' ) || exit;
