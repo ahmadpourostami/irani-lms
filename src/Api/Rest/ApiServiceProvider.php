b18e2d711002e0238cedcedf6cfcfdfa5a2ff327
ahmadpourostami <103726857+ahmadpourostami@users.noreply.github.com>
2026-08-24 07:59:46 +0330

    feat(api): register auth and student controllers

--- src/Api/Rest/ApiServiceProvider.php
+++ src/Api/Rest/ApiServiceProvider.php
@@ -10,12 +10,18 @@ defined( 'ABSPATH' ) || exit;
 
 final class ApiServiceProvider implements ServiceProviderInterface {
     private CourseController $courses;
+    private AuthController $auth;
+    private StudentController $student;
 
     public function register(): void {
         $this->courses = new CourseController();
+        $this->auth = new AuthController();
+        $this->student = new StudentController();
     }
 
     public function boot(): void {
         add_action( 'rest_api_init', [ $this->courses, 'register' ] );
+        add_action( 'rest_api_init', [ $this->auth, 'register' ] );
+        add_action( 'rest_api_init', [ $this->student, 'register' ] );
     }
 }
